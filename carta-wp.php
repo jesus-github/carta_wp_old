54af9b3ad25d432ad1b301f2402e0b6bc8726ebf
Jesus <jmartinez@synapse.es>
2021-11-07 12:47:02 +0100

    Añadido CPT platos

--- carta-wp.php
+++ carta-wp.php
@@ -1,11 +1,17 @@
 <?php
 
-/*
-Plugin Name: Carta Wp
-Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
-Description: A brief description of the Plugin.
-Version: 1.0
-Author: Jesús
-Author URI: http://URI_Of_The_Plugin_Author
-License: A "Slug" license name e.g. GPL2
-*/
+/**
+ * Plugin Name: Carta Wp
+ * Plugin URI: https://triplejota.com
+ * Description: Añade una carta con filtrado a la página de tu restaurante o cafetería.
+ * Version: 1.0
+ * Author: Jesús Martínez
+ * Author URI: https://triplejota.com
+ * Text Domain: jmd_platos
+ * License: GPL2
+ */
+
+/* Encolamos archivos */
+include plugin_dir_path(__FILE__) . 'includes/admin/functions.php';
+include plugin_dir_path(__FILE__) . 'includes/public/functions.php';
+include plugin_dir_path(__FILE__) . 'includes/schema/functions.php';
